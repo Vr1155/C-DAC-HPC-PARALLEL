09240b3e2bb1d174f8a3ad674a86040000e2f38e
Vr1155 <49389720+Vr1155@users.noreply.github.com>
2024-05-02 17:57:42 +0530

    Added code which were executed in lab into program folder

---
+++ d2/program/mat_mult.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<omp.h>
+#define ROW1 5000
+
+#define COL1 5000
+#define ROW2 5000
+#define COL2 5000
+float a[ROW1][COL1], b[ROW2][COL2], mul[ROW1][COL2];
+void multiply(int r1, int c1, int r2, int c2);
+int main()
+{
+
+int i,j,k,r1,c1,r2,c2;
+printf("Enter row and column of first matrix\n");
+scanf("%d%d", &r1, &c1);
+printf("Enter row and column of second matrix\n");
+scanf("%d%d", &r2, &c2);
+multiply(r1,c1,r2,c2);
+return 0;
+
+}
+
+void multiply(int r1, int c1, int r2, int c2)
+{
+
+int i,j,k;
+if(c1==r2)
+{
+printf("Enter elements of first matrix:\n");
+for(i=0;i< r1;i++)
+{
+for(j=0;j< c1;j++)
+ {
+    a[i][j]=1;
+
+ }
+
+
+}
+printf("Enter elements of second  matrix:\n");
+for(i=0;i< r2;i++)
+  {
+     for(j=0;j< c2;j++)
+        {
+                b[i][j]=1;
+        }
+
+ }
+#pragma omp parallel for schedule(dynamic ,1024)  private(i,j,k)
+for(i=0;i< r1;i++)
+ {
+     for(j=0;j< c2;j++)
+{
+                mul[i][j] = 0;
+                for(k=0;k< r2;k++)
+                    {
+                        mul[i][j] = mul[i][j] + a[i][k]*b[k][j];
+
+                    }
+        }
+}
+
+ printf("Multiplied matrix is:\n");
+   for(i=0;i< r1;i++)
+     {
+        for(j=0;j< c2;j++)
+           {
+
+             printf("%f\t", mul[i][j]);
+
+           }
+              printf("\n");
+
+     }
+
+
+}
+else
+{
+  printf("Dimension do not match for multiplication.");
+}
+
+}
+
+
---
+++ d2/program/mm.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <omp.h>
+#include <sys/time.h>
+#define SIZE 4000
+int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
+int main() {
+    int i, j, k;
+    struct timeval tv1, tv2;
+    struct timezone tz;
+        double elapsed;
+    for(i = 0; i < SIZE; i++) {
+        for(j = 0; j < SIZE; j++) {
+            A[i][j] = 1;
+            B[i][j] = 1;
+            C[i][j] = 0;
+        }
+    }
+    gettimeofday(&tv1, &tz);
+
+#pragma omp parallel for private(i, j, k)
+    for(i = 0; i < SIZE; i++) {
+        for(j = 0; j < SIZE; j++) {
+            for(k = 0; k < SIZE; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        } 
+    }
+
+   gettimeofday(&tv2, &tz);
+   elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
+   printf("elapsed time = %f seconds.\n", elapsed);
+
+    return 0;
+}
+
